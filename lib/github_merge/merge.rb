8f30144abaa2f9732e204b78acb86ae1219f3325
Arthur Maltson <arthur.kalm@gmail.com>
2013-04-02 23:42:14 -0400

    Clone repos based on YAML config

--- lib/github_merge/merge.rb
+++ lib/github_merge/merge.rb
@@ -10,5 +10,17 @@ class Merge
 
   def local!
     config = YAML.load_file @file
+    repo_dir = clone_repos(config)
+  end
+
+  def clone_repos(config)
+    repo_dir = "#{Dir.pwd}/out"
+    FileUtils.mkdir_p repo_dir
+    Dir.chdir repo_dir do
+      config["repositories"].each do |repo|
+        `git clone #{repo["url"]} #{repo["sub directory"]}`
+      end
+    end
+    repo_dir
   end
 end
