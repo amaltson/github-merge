315addddc2a656018a2350801bf2a44e7da19649
Arthur Maltson <arthur.kalm@gmail.com>
2013-04-03 22:28:35 -0400

    Move repositories to subdirectories in history
    Change the history of the cloned repositories and move them to the
    requested subdirectories.

--- lib/github_merge/merge.rb
+++ lib/github_merge/merge.rb
@@ -2,6 +2,7 @@ require 'yaml'
 
 class Merge
   VERSION = "1.0.0"
+  MOVE_TREE_SCRIPT_PATH = "#{Dir.pwd}/lib/github_merge"
 
   def initialize(file, options)
     @file = file
@@ -11,6 +12,7 @@ class Merge
   def local!
     config = YAML.load_file @file
     repo_dir = clone_repos(config)
+    move_repos_to_subdir(repo_dir, config)
   end
 
   def clone_repos(config)
@@ -22,5 +24,24 @@ class Merge
       end
     end
     repo_dir
+  end
+
+  def move_repos_to_subdir(repo_dir, config)
+    Dir.chdir repo_dir do
+      config["repositories"].each do |repo|
+        git_filter_branch_move(repo["sub directory"])
+      end
+    end
+  end
+
+  def git_filter_branch_move(subdir)
+    puts "Moving repository #{subdir} to subdirectory #{subdir}..."
+    Dir.chdir subdir do
+      %x[git filter-branch --index-filter \
+        'git ls-files -s | gsed "s@\t@&#{subdir}/@" |
+                GIT_INDEX_FILE=$GIT_INDEX_FILE.new \
+                        git update-index --index-info &&
+         mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"']
+    end
   end
 end
