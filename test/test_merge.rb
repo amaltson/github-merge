30be940d6c83a720b18659c8e0679c8642698f3e
Arthur Maltson <arthur.kalm@gmail.com>
2013-04-03 22:49:36 -0400

    Added tests

--- test/test_merge.rb
+++ test/test_merge.rb
@@ -1,8 +1,21 @@
-require "minitest/autorun"
-require "github_merge/merge"
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'github_merge/merge'
+require 'fileutils'
 
-class TestMerge < MiniTest::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+describe Merge do
+  before do
+    FileUtils.rm_r('out')
+    @merge = Merge.new 'test/sample-merge-config.yml', nil
+  end
+
+  it "should merge locally" do
+    @merge.local!
+
+    # Check clone and rename worked
+    %w{github-merge github}.each do |repo|
+      Dir.exists?("out/#{repo}").must_equal true
+      Dir.exists?("out/#{repo}/#{repo}").must_equal true
+    end
   end
 end
