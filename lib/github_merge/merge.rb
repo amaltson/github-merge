d54f1f5646049f401dbc93a44f4c51cf8d3ee63c
Arthur Maltson <arthur.kalm@gmail.com>
2013-04-07 23:25:08 -0400

    Initialize github object

--- lib/github_merge/merge.rb
+++ lib/github_merge/merge.rb
@@ -25,7 +25,18 @@ class Merge
   end
 
   def push!
+    github = initialize_github
+  end
 
+  def initialize_github
+    api_endpoint = config["host"] || "https://api.github.com"
+    oauth_token = config["oauth"]
+
+    Github.new do |config|
+      config.endpoint = api_endpoint
+      config.oauth_token = oauth_token
+      config.adapter = :net_http
+    end
   end
 
   def clone_repos(config)
