3c9ec6f8624d6256dba757a4a7fa20ebe5e3645f
Arthur Maltson <arthur.kalm@gmail.com>
2013-04-09 22:33:21 -0400

    Remove shell script that moves to subdir
    * managed to include shell script inside of Ruby

--- lib/github_merge/move-tree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git filter-branch --index-filter \
-        'git ls-files -s | gsed "s@\t@&'"$1"'/@" |
-                GIT_INDEX_FILE=$GIT_INDEX_FILE.new \
-                        git update-index --index-info &&
-         mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE"'
